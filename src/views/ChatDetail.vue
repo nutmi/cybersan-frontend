77e23e68f86057a119e555efb643d3ad15887867
Egor Riter <walnut@Egors-MBP.fritz.box>
2024-01-14 19:17:15 +0100

    changed website style

--- src/views/ChatDetail.vue
+++ src/views/ChatDetail.vue
@@ -1,153 +1,163 @@
 <template>
-    <body>
-      <div class="body-container">
-        <div class="info">
-            info about user
-            <div class="elem-1" v-if="showUser">
-                {{ displayedUser.username }}
-                <br>
-                {{ displayedUser.description }}
-                <br>
-                <ul>
-                  <li v-for="(achievement, id) in displayedUser.achievements" :key="id">
-                    {{ achievement.achivment.name }} - {{ achievement.achivment.description }}
-                  </li>
-                </ul>
-            </div>
-        </div>
-  
-        <div class="chat-container">
-          <div class="messages">
-            <div
-              v-for="message in messages"
-              :key="message.id"
-              :class="message.whos_message ? 'my-message' : 'message'"
-            >
-              <span class="author" @click="showUserData(message.user_display)">{{ message.name }}</span>
-              <br />
-              {{ message.text }}
-            </div>
+  <body>
+    <div class="body-container">
+      <div class="info">
+          info about user
+          <div class="elem-1" v-if="showUser">
+              {{ displayedUser.username }}
+              <br>
+              {{ displayedUser.description }}
+              <br>
+              <ul>
+                <li v-for="(achievement, id) in displayedUser.achievements" :key="id">
+                  {{ achievement.achivment.name }} - {{ achievement.achivment.description }}
+                </li>
+              </ul>
           </div>
-          <div class="input-container">
-            <input type="text" class="message-input" v-model="text_message" />
-            <button class="send-button" v-on:click="sendMessage">send</button>
-          </div>
-        </div>
-  
-  
-        <div class="elem-3">elem3</div>
       </div>
-    </body>
-  </template>
+
+      <div class="chat-container" id="chat-container">
+        <div class="messages" id="messages" ref="messagesContainer">
+          
+          <div
+            v-for="message in messages"
+            :key="message.id"
+            class="message"
+          >
+            <span class="author" @click="showUserData(message.user_display)">{{ message.user_display.username}}</span>
+            <span class="terminalName">@{{ message.user_display.terminalName}}:$ ~ </span>
+            {{ message.text }}
+          </div>
+          
+          <div class="message input">
+            <span class="author">{{ username }}</span>
+            <span class="terminalName"> {{ terminalName }}</span>
+            <input type="text" class="message-input" v-model="text_message"  @keydown.enter="sendMessage()"/>
+          </div>
+
+        </div>
+      </div>
+
+
+      <div class="elem-3">elem3</div>
+    </div>
+  </body>
+</template>
 
 <script>
-import axios from 'axios'
+import axios from 'axios';
 
 export default {
-    mounted() {
-        this.getId()
-        this.getMessage()
-        this.getUser()
-        this.timer = setInterval(this.getMessage, 5000)
+  mounted() {
+    this.getMessage();
+    this.getUser();
+    this.timer = setInterval(this.getMessage, 5000);
+  },
+  data() {
+    return {
+      user_id: '',
+      chat_id: this.$route.params.id,
+      messages: [],
+      text_message: '',
+      showUser: false,
+      displayedUser: {},
+      timer: '',
+      terminalName: '',
+      username: '',
+      user_data: {}
+    };
+  },
+  methods: {
+    scrollToBottom() {
+      const container = this.$refs.messagesContainer;
+      container.scrollTop = container.scrollHeight;
     },
-    data() {
-        return {
-            user_id: '',
-            chat_id: '',
-            messages: [],
-            text_message: '',
-            showUser: false,
-            displayedUser: {},
-            timer: ''
-        }
+    getMessage() {
+      const token = localStorage.getItem('token');
+      const apiUrl = `http://localhost:8000/message/?chatid=` + this.chat_id;
+
+      const config = token ? { headers: { Authorization: `Token ${token}` } } : {};
+
+      axios
+        .get(apiUrl, config)
+        .then((res) => {
+          this.messages = res.data;
+          this.$nextTick(() => {
+            this.scrollToBottom()
+          })
+        })
+        .catch((error) => {
+          console.error('Error fetching messages:', error);
+        });
     },
-    methods: {
-        getId() {
-            this.chat_id = this.$route.params.id
-        },
-        getMessage() {
-          const token = localStorage.getItem("token");
-          const apiUrl = `http://localhost:8000/message/?chatid=` + this.chat_id;
-
-          const config = token ? { headers: { Authorization: `Token ${token}` } } : {};
-
-          axios.get(apiUrl, config)
-              .then((res) => {
-                  this.messages = res.data;
-              })
-              .catch((error) => {
-                  console.error("Error fetching messages:", error);
-              });
-      },
-        getUser() {
-            const token = localStorage.getItem("token");
-            console.log(token);
-            if (token) {
-              axios
-                .get("http://127.0.0.1:8000/auth/users/me/", {
-                headers: {
-                    Authorization: `Token ${token}`,
-                },
-                })
-                .then((res) => {
-                console.log('user' + res.data.id);
-                this.user_id = res.data.id;
-                })
-                .catch((error) => {
-                console.error("Error fetching user:", error);
-                });
-            }
+    getUser() {
+      const token = localStorage.getItem('token');
+      if (token) {
+        axios
+          .get('http://127.0.0.1:8000/auth/users/me/', {
+            headers: {
+              Authorization: `Token ${token}`,
+            },
+          })
+          .then((res) => {
+            this.user_id = res.data.id;
+            this.terminalName = res.data.terminalName;
+            this.username = res.data.username;
+            this.user_data = {username: res.data.username, terminalName: res.data.terminalName, userId: res.data.id}
+          })
+          .catch((error) => {
+            console.error('Error fetching user:', error);
+          });
+      }
     },
-        sendMessage() {
-            if (!this.user_id) {
-                console.error("User ID not available. Unable to send message.");
-                return;
-            }
-            
-            const token = localStorage.getItem("token");
-            axios
-                .post(
-                "http://127.0.0.1:8000/message/",
-                {
-                    text: this.text_message,
-                    user: this.user_id,
-                    chat: this.chat_id
+    sendMessage() {
+      if (!this.user_id) {
+        console.error('User ID not available. Unable to send message.');
+        return;
+      }
 
-                },
-                {
-                    headers: {
-                    Authorization: `Token ${token}`,
-                    },
-                }
-                )
-                .then((res) =>{
-                    console.log(res.data)
-                    this.text_message = ''
-                })
-                .catch((error) => {
-                console.error("Error sending message:", error);
-                });
-        },
-        showUserData(userData) {
-            this.showUser = !this.showUser
-            this.displayedUser = userData
-            console.log(userData)
-        }
-    }
-}
+      const token = localStorage.getItem('token');
+      axios
+        .post(
+          'http://127.0.0.1:8000/message/',
+          {
+            text: this.text_message,
+            user: this.user_id,
+            chat: this.chat_id,
+          },
+          {
+            headers: {
+              Authorization: `Token ${token}`,
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res.data);
+          this.text_message = '';
+          this.getMessage(); // Fetch messages after sending a new one
+        })
+        .catch((error) => {
+          console.error('Error sending message:', error);
+        });
+    },
+    showUserData(userData) {
+      this.showUser = !this.showUser;
+      this.displayedUser = userData;
+    },
+  },
+};
 </script>
+
 
 <style scoped>
 body {
   margin: 0;
   padding: 0;
-  font-family: Arial, sans-serif;
-  display: flex;
   flex-direction: column;
   align-items: center;
   height: 100vh;
+  background-color: #fefefe; /* Light background color */
 }
-
 
 .body-container {
   display: flex;
@@ -155,92 +165,86 @@ body {
   justify-content: space-between;
   height: 100%;
   width: 100%;
-  background-color: white;
+  background-color: #fefefe;
 }
-
 
 .info {
   background-color: #00000077;
   height: 20%;
   width: 15%;
+  padding: 10px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5); /* Box shadow for a bit of depth */
 }
 
 .elem-1 {
-    color: #ffd5009f;
+  color: #ffd5009f;
 }
+
 .chat-container {
   display: flex;
   flex-direction: column;
-  background-color:grey;
+  background-color: #413b3b; /* Light grey background */
   width: 40%;
   height: 90%;
+  border: 2px solid #00000077; /* Black border */
+  border-radius: 10px; /* Rounded corners */
+  overflow: hidden; /* Hide overflowing content */
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.5); /* Box shadow for a bit of depth */
 }
-
 
 .elem-3 {
-  background-color: aqua;
+  background-color: #3498db; /* Blue color for elem-3 */
+  border-radius: 5px; /* Rounded corners */
+  padding: 10px;
+  margin-top: 10px;
+  color: white;
 }
-
 
 .messages {
   height: 93%;
-  background-color: #00000077;
+  background-color: #5a4e4e;
   overflow-y: auto;
   display: flex;
-  flex-direction: column; /* Align messages to the bottom */
-  justify-content: flex-start;
+  flex-direction: column;
+  align-items: start;
+  padding: 10px; /* Add some padding inside the messages container */
 }
-
 
 .message {
-  background-color: #b1bbc146;
-  color: #fff;
+ /* Red color for messages */
+  color: #d39a1e;
   padding: 10px;
   margin-bottom: 5px;
   border-radius: 5px;
-  font-size: 12px;
-  text-align: start;
-}
-.my-message {
-  background-color: rgba(99, 104, 106, 0.368);
-  color: #fff;
-  padding: 10px;
-  margin-bottom: 5px;
-  border-radius: 5px;
-  font-size: 12px;
+  font-size: 14px; /* Slightly larger font size */
   text-align: end;
+;
 }
 
-
-.input-container {
-  display: flex;
-  background-color: #80808078;
-  height: 7%;
-  align-items: center;
-}
 .message-input {
+  color: #d39a1e;
+  background-color: #5a4e4e0a;
   flex: 1;
   padding: 8px;
   margin-right: 10px;
-  font-size: 10px;
+  font-size: 14px;
+  border: 0;
 }
 
-
-.send-button {
-  padding: 8px 15px;
-  background-color: #00000077;
-  color: yellow;
-  cursor: pointer;
-  font-size: 10px;
-    border: none;
+.message-input:focus {
+  outline: none;
 }
-
 
 .author {
   font-weight: bold;
   margin-right: 5px;
-  color: #ffd5009f;
+  color: #ff62009f;
 }
+
+.terminalName {
+  color: aqua;
+}
+
 </style>
 
 
